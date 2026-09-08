165c6509443d5dab1186d1ea150cfc63e8628761
zeenatparweenz <20dpcs014hy@manuu.edu.in>
2026-09-08 14:13:58 +0530

    Auto Commit

---
+++ printName.java
@@ -0,0 +1,8 @@
+public class printName{
+    public static void main(String[]args){
+        //Replace "Alex" with the name you want to print
+        System.out.println("Alex");
+    }
+
+    }
+}
--- weet.java
+++
@@ -1,6 +0,0 @@
-public class weet {
-pulic static void main(String[]args){
-    System.out.println(call me januu);
-}
-    
-}
