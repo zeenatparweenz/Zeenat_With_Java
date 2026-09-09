c180cbec99f299f7c9c8296169a236b4845a11f3
zeenatparweenz <20dpcs014hy@manuu.edu.in>
2026-09-09 10:25:59 +0530

    Auto Commit

--- example.java
+++ example.java
@@ -1,7 +1,15 @@
 class example
 {
+    void fun1()
+    {
+        System.out.println("A");
+    }
+    void fun2()
+    {
+        System.out.println("B");
+    }
     public static void main(String[] args) {
-        System.out.println(Hello);
+        System.out.println("hello");
 
     }
 }
