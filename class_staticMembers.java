a34fd4b71899ec88e73f80802ded20e3b7a920bc
zeenatparweenz <20dpcs014hy@manuu.edu.in>
2026-09-12 22:43:26 +0530

    Auto Commit

--- class_staticMembers.java
+++ class_staticMembers.java
@@ -14,18 +14,19 @@ class A{
         y=45;
     }
 
-   {
+}
     class staticMembers{
-    public static void main(String []args)
+    public static void main(String []args) 
     {
 A a1=new A();
 A a2=new A();
+
 a1.fun1();
 a2.fun3();
+
 a1.fun2();
     }
 }    
-   }
-
+   
 
 
