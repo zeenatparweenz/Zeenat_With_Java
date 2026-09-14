2642ce58f40ae47eb3fe9f746edc7445679ddb6f
zeenatparweenz <20dpcs014hy@manuu.edu.in>
2026-09-14 12:12:20 +0530

    Auto Commit

---
+++ EvenOrOddNumber.java
@@ -0,0 +1,9 @@
+class EvenOrOddNumber{
+    public static void main(String[] args) {
+        int n = 10;
+        if(n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
