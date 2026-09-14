72ae427b097544a52f03953bedf002980daf4234
zeenatparweenz <20dpcs014hy@manuu.edu.in>
2026-09-14 11:52:34 +0530

    Auto Commit

---
+++ TakeInputFromUser.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+
+        System.err.print("Enter Your Name");
+        String name = sc.nextLine();
+        System.out.println("Hello" + name);
+
+    }
+
+
+}
