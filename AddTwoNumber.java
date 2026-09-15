e6a72ebd3e25c8f8fb4504d7cfac263f8983abd9
zeenatparweenz <20dpcs014hy@manuu.edu.in>
2026-09-15 11:33:11 +0530

    Auto Commit

--- AddTwoNumber.java
+++ AddTwoNumber.java
@@ -1,7 +1,7 @@
 class AddTwoNumber{
     public static void main(String[] args) {
-        inta = 20;
-        intb = 40;
+        int a = 20;
+        int b = 40;
         int sum = a+b;
         System.out.println("sum ="+ sum);
     }
