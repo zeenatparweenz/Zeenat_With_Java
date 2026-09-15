1f1b46fda3037cce74ae5c3d4b9e2723bfb75108
zeenatparweenz <20dpcs014hy@manuu.edu.in>
2026-09-15 11:37:41 +0530

    Auto Commit

---
+++ FindGreaterNumber.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+        if(a > b)
+            System.out.println("A is greater");
+        else
+            System.out.println("B is greater");
+    }
+}
