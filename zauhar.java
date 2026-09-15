88c9f9ac225fa9d6ac5e168fb9af4d1cca476c10
zeenatparweenz <20dpcs014hy@manuu.edu.in>
2026-09-15 11:44:27 +0530

    Auto Commit

---
+++ zauhar.java
@@ -0,0 +1,11 @@
+class zauhar {
+        public static void main(String[] args) {
+
+                String name = "Zauhar";
+                        int age = 22;
+
+                                System.out.println("Name = " + name);
+                                        System.out.println("Age = " + age);
+                                            }
+                                            }
+}
